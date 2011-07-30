c24f8efe1163b89b714b5df1d2c3ba6dd7aea0ee
Prakash Surya <surya1@llnl.gov>
2011-07-29 19:13:31 -0700

    Programming Project 4 - CS143 - Compilers
    Initial commit of the pp4 directory copied from:
    myth.stanford.edu:/usr/class/cs143/assignments/pp4

---
+++ ast_stmt.cc
@@ -0,0 +1,67 @@
+/* File: ast_stmt.cc
+ * -----------------
+ * Implementation of statement node classes.
+ */
+#include "ast_stmt.h"
+#include "ast_type.h"
+#include "ast_decl.h"
+#include "ast_expr.h"
+
+
+Program::Program(List<Decl*> *d) {
+    Assert(d != NULL);
+    (decls=d)->SetParentAll(this);
+}
+
+void Program::Check() {
+    /* You can use your pp3 semantic analysis or leave it out if
+     * you want to avoid the clutter.  We won't test pp4 against 
+     * semantically-invalid programs.
+     */
+}
+void Program::Emit() {
+    /* pp4: here is where the code generation is kicked off.
+     *      The general idea is perform a tree traversal of the
+     *      entire program, generating instructions as you go.
+     *      Each node can have its own way of translating itself,
+     *      which makes for a great use of inheritance and
+     *      polymorphism in the node classes.
+     */
+}
+
+StmtBlock::StmtBlock(List<VarDecl*> *d, List<Stmt*> *s) {
+    Assert(d != NULL && s != NULL);
+    (decls=d)->SetParentAll(this);
+    (stmts=s)->SetParentAll(this);
+}
+
+ConditionalStmt::ConditionalStmt(Expr *t, Stmt *b) { 
+    Assert(t != NULL && b != NULL);
+    (test=t)->SetParent(this); 
+    (body=b)->SetParent(this);
+}
+
+ForStmt::ForStmt(Expr *i, Expr *t, Expr *s, Stmt *b): LoopStmt(t, b) { 
+    Assert(i != NULL && t != NULL && s != NULL && b != NULL);
+    (init=i)->SetParent(this);
+    (step=s)->SetParent(this);
+}
+
+IfStmt::IfStmt(Expr *t, Stmt *tb, Stmt *eb): ConditionalStmt(t, tb) { 
+    Assert(t != NULL && tb != NULL); // else can be NULL
+    elseBody = eb;
+    if (elseBody) elseBody->SetParent(this);
+}
+
+
+ReturnStmt::ReturnStmt(yyltype loc, Expr *e) : Stmt(loc) { 
+    Assert(e != NULL);
+    (expr=e)->SetParent(this);
+}
+  
+PrintStmt::PrintStmt(List<Expr*> *a) {    
+    Assert(a != NULL);
+    (args=a)->SetParentAll(this);
+}
+
+
