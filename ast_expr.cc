b10fe268aa9cdf658f27008aa48d3afdcb837dba
Prakash Surya <surya1@llnl.gov>
2011-07-31 15:10:19 -0700

    Implement Emit method for constant expressions
    Added an implementation for the Emit method for the constant expression classes
    (i.e. IntConstant, BoolConstant, etc.).

--- ast_expr.cc
+++ ast_expr.cc
@@ -7,22 +7,52 @@
 #include "ast_expr.h"
 #include "ast_type.h"
 #include "ast_decl.h"
+#include "codegen.h"
 
 IntConstant::IntConstant(yyltype loc, int val) : Expr(loc) {
     value = val;
+}
+
+Location* IntConstant::Emit(CodeGenerator *cg) {
+    return cg->GenLoadConstant(value);
 }
 
 DoubleConstant::DoubleConstant(yyltype loc, double val) : Expr(loc) {
     value = val;
 }
 
+Location* DoubleConstant::Emit(CodeGenerator *cg) {
+    /* From the PP4 assignment description (page 3):
+     * "We have removed doubles from the type of Decaf for this project"
+     * Thus, Assert if this is encountered.
+     */
+    Assert(0);
+    return NULL;
+}
+
 BoolConstant::BoolConstant(yyltype loc, bool val) : Expr(loc) {
     value = val;
+}
+
+Location* BoolConstant::Emit(CodeGenerator *cg) {
+    /* From the PP4 assignment description (page 3):
+     * "We treat bools just like ordinary 4-byte integers, which evaluate to 0
+     * or not 0.
+     */
+    return cg->GenLoadConstant(value ? 1 : 0);
 }
 
 StringConstant::StringConstant(yyltype loc, const char *val) : Expr(loc) {
     Assert(val != NULL);
     value = strdup(val);
+}
+
+Location* StringConstant::Emit(CodeGenerator *cg) {
+    return cg->GenLoadConstant(value);
+}
+
+Location* NullConstant::Emit(CodeGenerator *cg) {
+    return cg->GenLoadConstant(0);
 }
 
 Operator::Operator(yyltype loc, const char *tok) : Node(loc) {
